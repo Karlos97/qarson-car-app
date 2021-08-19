a75ccff3f63105886a253d995b472f92d83fabdb
ads <asd>
2021-08-19 06:48:10 -0700

    Basic settings etc.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Table from './components/MainTable/Table';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        This is recruitment task for Qarson company.
       </header>
+      <Table/>
     </div>
   );
 }
